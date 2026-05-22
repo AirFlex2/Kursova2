afe7d908d48f8a0a4c6f82c522280599ea06667c
AirFlex2 <tymur.zinkivskyi@nure.ua>
2026-05-23 02:49:10 +0300

    last fix?

--- Kursova2.Server/Services/Microservices/AnalyticsMicroservice.cs
+++ Kursova2.Server/Services/Microservices/AnalyticsMicroservice.cs
@@ -1,5 +1,5 @@
 ﻿using System.Data;
-using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
 using Kursova2.Services.Database;
 
 namespace Kursova2.Services.Microservices
--- Kursova2.Server/Services/Microservices/BookMicroservice.cs
+++ Kursova2.Server/Services/Microservices/BookMicroservice.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
-using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
 using Kursova2.Models;
 using Kursova2.Services.Database;
 
